e75e07db366af57c1d2b8e71288f7c63c76036e1
ButterOnPancakes <charlielaurent29@gmail.com>
2025-02-17 15:09:04 +0100

    changed directories (MAKE CLEAN)

--- src/display.c
+++ src/display.c
@@ -16,30 +16,31 @@ SDL_Texture* load_texture(const char* path, SDL_Renderer* renderer) {
 
 void load_sprites(SDL_Renderer* ren) {
     sl = malloc(sizeof(sprite_list));
-    sl->player_texture = load_texture("assets/player/sprite_sheet.bmp", ren);
+    sl->player_texture = load_texture("assets/entities/player.bmp", ren);
+    sl->alien_texture = load_texture("assets/entities/alien.bmp", ren);
+    sl->goblin_texture = load_texture("assets/entities/goblin.bmp", ren);
+    sl->slime_texture = load_texture("assets/entities/slime.bmp", ren);
+
     sl->inv_texture = load_texture("assets/inventory/inv_grid.bmp", ren);
     sl->apple_item_texture = load_texture("assets/inventory/apple.bmp", ren);
-    sl->alien_texture = load_texture("assets/alien/sprite_sheet.bmp", ren);
-    sl->sword_texture = load_texture("assets/sword/spritesheet.bmp", ren);
-    sl->goblin_texture = load_texture("assets/goblin/sprite_sheet.bmp", ren);
-    sl->sword_slash = load_texture("assets/player/sword.bmp", ren);
-    sl->cobble_texture = load_texture("assets/tilemap/cobble.bmp", ren);
-    sl->iconE_texture = load_texture("assets/tilemap/iconE.bmp", ren);
-    sl->teleporter_texture = load_texture("assets/tilemap/teleporter.bmp", ren);
-    sl->red_heart_full_texture = load_texture("assets/health/red_heart_full.bmp", ren);
-    sl->red_heart_half_texture = load_texture("assets/health/red_heart_half.bmp", ren);
-    sl->heart_empty_texture = load_texture("assets/health/heart_empty.bmp", ren);
-    sl->chest_opened_texture = load_texture("assets/tilemap/chest_opened.bmp", ren);
-    sl->chest_closed_texture = load_texture("assets/tilemap/chest_closed.bmp", ren);
     sl->unknown_item_texture = load_texture("assets/inventory/unknown_item.bmp", ren);
     sl->sword_item_texture = load_texture("assets/inventory/sword_item.bmp", ren);
-    sl->background_texture = load_texture("assets/tilemap/background.bmp", ren);
+
+    sl->sword_texture = load_texture("assets/sword/spritesheet.bmp", ren);
     sl->sword_up_texture = load_texture("assets/sword/spritesheet_up.bmp", ren);
     sl->sword_down_texture = load_texture("assets/sword/spritesheet_down.bmp", ren);
     sl->sword_mirror_texture = load_texture("assets/sword/spritesheet_mirror.bmp", ren);
 
-    sl->slime_texture = load_texture("assets/alien/slime.bmp", ren);
-    sl->slime_jump_texture = load_texture("assets/alien/slime.bmp", ren);
+    sl->cobble_texture = load_texture("assets/tilemap/cobble.bmp", ren);
+    sl->iconE_texture = load_texture("assets/tilemap/iconE.bmp", ren);
+    sl->teleporter_texture = load_texture("assets/tilemap/teleporter.bmp", ren);
+    sl->chest_opened_texture = load_texture("assets/tilemap/chest_opened.bmp", ren);
+    sl->chest_closed_texture = load_texture("assets/tilemap/chest_closed.bmp", ren);
+    sl->background_texture = load_texture("assets/tilemap/background.bmp", ren);
+
+    sl->red_heart_full_texture = load_texture("assets/health/red_heart_full.bmp", ren);
+    sl->red_heart_half_texture = load_texture("assets/health/red_heart_half.bmp", ren);
+    sl->heart_empty_texture = load_texture("assets/health/heart_empty.bmp", ren);
 }
 
 sprite_list* get_sprites() {
--- src/display.h
+++ src/display.h
@@ -3,31 +3,31 @@
 #include <SDL2/SDL.h>
 
 typedef struct {
-
     SDL_Texture* player_texture;
     SDL_Texture* alien_texture;
     SDL_Texture* goblin_texture;
-    SDL_Texture* apple_item_texture;
-    SDL_Texture* sword_slash;
-    SDL_Texture* cobble_texture;
-    SDL_Texture* iconE_texture;
-    SDL_Texture* teleporter_texture;
+    SDL_Texture* slime_texture;
+
     SDL_Texture* inv_texture;
-    SDL_Texture* red_heart_full_texture;
-    SDL_Texture* red_heart_half_texture;
-    SDL_Texture* heart_empty_texture;
-    SDL_Texture* chest_opened_texture;
-    SDL_Texture* chest_closed_texture;
+    SDL_Texture* apple_item_texture;
     SDL_Texture* unknown_item_texture;
     SDL_Texture* sword_item_texture;
-    SDL_Texture* background_texture;
+
     SDL_Texture* sword_texture;
     SDL_Texture* sword_up_texture;
     SDL_Texture* sword_down_texture;
     SDL_Texture* sword_mirror_texture;
 
-    SDL_Texture* slime_texture;
-    SDL_Texture* slime_jump_texture;
+    SDL_Texture* cobble_texture;
+    SDL_Texture* iconE_texture;
+    SDL_Texture* teleporter_texture;
+    SDL_Texture* chest_opened_texture;
+    SDL_Texture* chest_closed_texture;
+    SDL_Texture* background_texture;
+
+    SDL_Texture* red_heart_full_texture;
+    SDL_Texture* red_heart_half_texture;
+    SDL_Texture* heart_empty_texture;
 } sprite_list;
 
 void load_sprites(SDL_Renderer* ren);
